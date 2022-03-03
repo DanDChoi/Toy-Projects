8c83ebeaed58b5980e5a85d626ef2349a47b07fd
DanDChoi <showbegin622@gmail.com>
2022-03-03 17:31:13 +0900

    black_jack

---
+++ Gamer.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+public class Gamer {
+    private List<Card> cards;
+
+    public void receiveCard(Card card){
+
+    }
+    public List<Card> openCards(){
+        return null;
+    }
+}
