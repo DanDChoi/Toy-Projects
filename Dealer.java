f4b0306733a5b2fe07041c42bea7044fd0d24bf0
DanDChoi <showbegin622@gmail.com>
2022-04-30 22:50:36 +0900

    Fix Dealer

--- Dealer.java
+++ Dealer.java
@@ -3,7 +3,14 @@ import java.util.List;
 public class Dealer {
     private List<Card> cards;
 
+    private static final int CAN_RECEIVE_POINT = 16;
+
+    public Dealer(){
+        cards = new ArrayList<>();
+    }
+
     public void receiveCard(Card card){
+        
 
     }
     public List<Card> openCards(){
