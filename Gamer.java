3c22474d05b3736566c6cb31ce1481c913fe4a92
DanDChoi <showbegin622@gmail.com>
2022-05-01 23:44:19 +0900

    add Gamer

--- Gamer.java
+++ Gamer.java
@@ -4,9 +4,22 @@ public class Gamer {
     private List<Card> cards;
 
     public void receiveCard(Card card){
-
+        this.cards.add(card);
     }
+
     public List<Card> openCards(){
         return null;
     }
+    
+    public void showCards(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("현재 보유카드 목록 \n");
+
+        for(Card card : cards){
+            sb.append(card.toString());
+            sb.append("\n");
+        }
+        System.out.println(sb.toString());
+    }
+
 }
