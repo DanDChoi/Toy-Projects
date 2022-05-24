03868fc618437cb0e9f88c361f2310ddf78d3d0c
DanDChoi <showbegin622@gmail.com>
2022-05-25 01:39:54 +0900

    adding EnumModel

--- src/main/java/com/example/demo/after/EnumContract.java
+++ src/main/java/com/example/demo/after/EnumContract.java
@@ -22,7 +22,7 @@ public class EnumContract {
     @Enumerated(EnumType.STRING)
     private CommissionCutting commissionCutting; //수수료 절삭 (ex: 반올림, 올림, 버림)
 
-    public enum CommissionType {
+    public enum CommissionType implements EnumModel{
         PERCENT("percent"),
         MONEY("money");
 
@@ -32,16 +32,17 @@ public class EnumContract {
             this.value = value;
         }
 
+        @Override
         public String getKey(){
             return name();
         }
-
+        @Override
         public String getValue(){
             return value;
         }
     }
 
-    public enum CommissionCutting{
+    public enum CommissionCutting implements EnumModel{
         ROUND("round"),
         CEIL("ceil"),
         FLOOR("floor");
@@ -51,11 +52,11 @@ public class EnumContract {
         CommissionCutting(String value){
             this.value = value;
         }
-
+        @Override
         public String getKey(){
             return name();
         }
-
+        @Override
         public String getValue(){
             return value;
         }
