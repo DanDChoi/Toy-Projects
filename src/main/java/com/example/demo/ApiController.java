4f52798a394e233102213dd3aba7f36a7df114e8
DanDChoi <showbegin622@gmail.com>
2022-05-26 14:10:37 +0900

    add EnumValue, change Controller

--- src/main/java/com/example/demo/ApiController.java
+++ src/main/java/com/example/demo/ApiController.java
@@ -1,11 +1,13 @@
 package com.example.demo;
 
 import com.example.demo.after.EnumContract;
+import com.example.demo.after.EnumModel;
+import com.example.demo.after.EnumValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @RestController
 public class ApiController {
@@ -20,6 +22,28 @@ public class ApiController {
         enums.put("commissionType", commissionType.getEnumConstants());
         enums.put("commissionCutting", commissionCutting.getEnumConstants());
         return enums;
+    }
+
+    @GetMapping
+    public Map<String, List<EnumValue>> getEnumValue(){
+        Map<String, List<EnumValue>> enumValues = new LinkedHashMap<>();
+
+        enumValues.put("commissionType", toEnumValues(EnumContract.CommissionType.class));
+        enumValues.put("commissionCutting", toEnumValues(EnumContract.CommissionCutting.class));
+
+        return enumValues;
+    }
+
+    private List<EnumValue> toEnumValues(Class<? extends EnumModel> e){
+        return Arrays.stream(e.getEnumConstants())
+                .map(EnumValue::new)
+                .collect(Collectors.toList());
+//        Java8이 아닌 경우
+//        List<EnumValue> enumValues = new ArrayList<>();
+//        for (EnumModel enumType : e.getEnumConstants()){
+//            enumValues.add(new EnumValue(enumType));
+//        }
+//        return enumValues;
     }
 
 }
