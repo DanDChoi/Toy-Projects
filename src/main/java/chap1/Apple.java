5f92aee062f306b6c22da5d210d118eca12b5fa0
DanDChoi <showbegin622@gmail.com>
2022-05-29 14:47:37 +0900

    parameter에 method를 받기 step1

--- src/main/java/chap1/Apple.java
+++ src/main/java/chap1/Apple.java
@@ -1,6 +1,18 @@
 package chap1;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
+@NoArgsConstructor
 public class Apple {
+    private String color;
+    private int weight;
 
+    @Builder
+    public Apple(String color, int weight){
+        this.color = color;
+        this.weight = weight;
+    }
 }
--- src/main/java/chap1/FruitInventory.java
+++ src/main/java/chap1/FruitInventory.java
@@ -1,8 +1,31 @@
 package chap1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FruitInventory {
 
+    //녹색 사과를 찾고싶다!
     public static List<Apple> filterGreenApples(List<Apple> inventory){
+        List<Apple> result = new ArrayList<>();
 
+        for(Apple apple : inventory){
+            if("green".equals(apple.getColor())){
+                result.add(apple);
+            }
+        }
+        return result;
+    }
+
+    //만약 빨간색사과도 찾고싶다면?
+    public static List<Apple> filterApplesByColor(List<Apple> inventory, String color){ //(1)파라미터에 color를 받아
+        List<Apple> result = new ArrayList<>();
+
+        for(Apple apple : inventory){
+            if(apple.getColor().equals(color)){ // (2)비교조건에 사용
+                result.add(apple);
+            }
+        }
+        return result;
     }
 }
