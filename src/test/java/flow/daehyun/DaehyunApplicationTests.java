fb3f3bd19a1211889fb33cf21c0e946d94279796
DanDChoi <showbegin622@gmail.com>
2022-08-06 15:02:20 +0900

    project setting

---
+++ src/test/java/flow/daehyun/DaehyunApplicationTests.java
@@ -0,0 +1,13 @@
+package flow.daehyun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaehyunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
