db19d2e3799e30d5ccab7bb75d809708307d6298
Dan <danchoi.dev@gmail.com>
2024-06-22 20:56:32 +0900

    equals hashCode 사용하기

---
+++ business-logic/src/main/java/business_logic/Dan/rowsum/domain/Payment.java
@@ -0,0 +1,59 @@
+package business_logic.Dan.rowsum.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Payment {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column
+    private Long ownerId; // 업주 Id
+
+    @Column
+    private LocalDate payDate; // 결제일
+
+    @Column
+    private String calculateCode; // 계산방식을 선택하는 계산코드
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    private Method method; // 결제방식
+
+    @Column
+    private int price;
+
+    @Builder
+    public Payment(Long ownerId, LocalDate payDate, String calculateCode, Method method, int price) {
+        this.ownerId = ownerId;
+        this.payDate = payDate;
+        this.calculateCode = calculateCode;
+        this.method = method;
+        this.price = price;
+    }
+
+    public enum Method {
+        MOBILE("휴대폰"),
+        CREDIT_CARD("신용카드"),
+        CASH("현금");
+
+        private String text;
+
+        Method(String text) {
+            this.text = text;
+        }
+
+        public String getText() {
+            return text;
+        }
+    }
+}
---
+++ business-logic/src/main/java/business_logic/Dan/rowsum/domain/Sales.java
@@ -0,0 +1,60 @@
+package business_logic.Dan.rowsum.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class Sales {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column
+    private Long ownerId;
+
+    @Column
+    private LocalDate payDate;
+
+    @Column
+    private String calculateCode;
+
+    @Column
+    private int totalAmount;
+
+    @Column
+    private int mobileAmount;
+
+    @Column
+    private int creditCardAmount;
+
+    @Column
+    private int cashAmount;
+
+    @Builder
+    public Sales(Long ownerId, LocalDate payDate, String calculateCode, int totalAmount, int mobileAmount, int creditCardAmount, int cashAmount) {
+        this.ownerId = ownerId;
+        this.payDate = payDate;
+        this.calculateCode = calculateCode;
+        this.totalAmount = totalAmount;
+        this.mobileAmount = mobileAmount;
+        this.creditCardAmount = creditCardAmount;
+        this.cashAmount = cashAmount;
+    }
+
+    public void add(int amount, Payment.Method paymentMethod){
+        if(paymentMethod == Payment.Method.MOBILE){ mobileAmount = amount; }
+        else if(paymentMethod == Payment.Method.CREDIT_CARD){ creditCardAmount = amount; }
+        else if(paymentMethod == Payment.Method.CASH) { cashAmount = amount; }
+        totalAmount += amount;
+    }
+}
