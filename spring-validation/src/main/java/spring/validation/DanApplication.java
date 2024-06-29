877e451b05d6f326ce8c1b7cd1be1f0e9524358d
Dan <danchoi.dev@gmail.com>
2024-06-29 21:43:51 +0900

    spring-validation common module first commit

---
+++ spring-validation/src/main/java/spring/validation/DanApplication.java
@@ -0,0 +1,13 @@
+package spring.validation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanApplication.class, args);
+	}
+
+}
