d7944cd707315bfed62452eb95a55e3215f3e2ea
Dan <danchoi.dev@gmail.com>
2024-07-04 21:47:24 +0900

    0704

--- spring-validation/src/main/java/spring/validation/DanApplication.java
+++ spring-validation/src/main/java/spring/validation/DanApplication.java
@@ -2,9 +2,34 @@ package spring.validation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.reactive.error.DefaultErrorAttributes;
+import org.springframework.boot.web.reactive.error.ErrorAttributes;
+import org.springframework.context.annotation.Bean;
+import spring.validation.domain.exception.ValidCustomException;
+
+import java.util.Map;
 
 @SpringBootApplication
 public class DanApplication {
+
+	@Bean
+	public ErrorAttributes errorAttributes() {
+		return new DefaultErrorAttributes() {
+
+			@Override
+			public Map<String, Object> getErrorAttributes(
+					RequestAttributes requestAttributes,
+					boolean includeStackTrace) {
+				Map<String, Object> errorAttributes = super.getErrorAttributes(requestAttributes, includeStackTrace);
+				Throwable error = getError(requestAttributes);
+				if (error instanceof ValidCustomException) {
+					errorAttributes.put("errors", ((ValidCustomException)error).getErrors());
+				}
+				return errorAttributes;
+			}
+
+		};
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DanApplication.class, args);
